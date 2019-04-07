049c54e89cf5641e370d7afa80432eb9c2383b47
Sergio Fenoll <sergio@fenoll.be>
2019-04-07 19:08:04 +0200

    Added waifu command, which fetches a randomly generated waifu from the thiswaifudoesnotexist.net

---
+++ plugins/waifu.py
@@ -0,0 +1,24 @@
+import io
+import aiohttp
+import random
+import discord
+from discord.ext import commands
+
+class Waifu:
+    def __init__(self, bot):
+        self.bot = bot
+        self.url = "https://www.thiswaifudoesnotexist.net/example-{}.jpg"
+
+    @commands.command()
+    async def waifu(self, ctx):
+        num = random.randint(0, 99999)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url.format(num)) as r:
+                if r.status != 200:
+                    return await ctx.message.channel.send("I couldn't get you a waifu...")
+                img = io.BytesIO(await r.read())
+                await ctx.message.channel.send(file=discord.File(img, f'waifu-{num}.jpg'))
+
+
+def setup(bot):
+    bot.add_cog(Waifu(bot))
