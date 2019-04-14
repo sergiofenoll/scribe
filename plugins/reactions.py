126b74d949f2d9f9c68dde73ace236dc193f3ee7
Sergio Fenoll <sergio@fenoll.be>
2019-04-14 18:19:12 +0200

    Added dog multiplier

--- plugins/reactions.py
+++ plugins/reactions.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import re
 import discord
@@ -7,13 +8,23 @@ from math import ceil
 class Reactions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.max_dog = 3
+        self.dog_multiplier = None
+
+    async def increment_dog_multiplier(self):
+        self.max_dog += 1
+        await asyncio.sleep(10)
+        self.max_dog = 3
 
     @commands.command()
     async def dog(self, ctx, aaa=""):
         if re.match("a+", aaa):
-            amt = min(ceil(len(aaa) / 3), 3)
+            amt = min(ceil(len(aaa) / 3), self.max_dog)
         else:
             amt = 1
+        if self.dog_multiplier is not None:
+            self.dog_multiplier.cancel()
+        self.dog_multiplier = asyncio.create_task(self.increment_dog_multiplier())
         for _ in range(amt):
             await ctx.message.channel.send(file=discord.File(os.path.join(os.path.dirname(__file__), "..", "static" ,"dogaaaa.png")))
         
