c4d58e4c6431d43908b473ae220bfe193f5035ff
Sergio Fenoll <sergio@fenoll.be>
2019-06-23 11:45:55 +0200

    Fixes bug in $aaaaaa where _all_ commands resulted in a dog bein sent

--- plugins/reactions.py
+++ plugins/reactions.py
@@ -19,11 +19,11 @@ class Reactions(commands.Cog):
         await asyncio.sleep(10)
         self.max_dog = 3
 
-    async def send_dogs(self, message, aaa):
+    async def send_dogs(self, message, aaa, min_amt=1):
         if re.match("a+", aaa, re.IGNORECASE):
             amt = min(ceil(len(aaa) / 3), self.max_dog)
         else:
-            amt = 1
+            amt = min_amt
         if self.dog_multiplier is not None:
             self.dog_multiplier.cancel()
         self.dog_multiplier = asyncio.create_task(self.increment_dog_multiplier())
@@ -39,7 +39,7 @@ class Reactions(commands.Cog):
         if message.content.startswith(self.bot.command_prefix):
             # Get the first "word" from the message and remove command_prefix
             aaa = message.content.split(' ', 1)[0].strip(self.bot.command_prefix)
-            await self.send_dogs(message, aaa)
+            await self.send_dogs(message, aaa, min_amt=0)
 
 
 def setup(bot):
