92e1fa9ec547fed3d0e44281f646b4ea758179bb
Sergio Fenoll <sergio@fenoll.be>
2019-06-28 00:00:25 +0200

    Adds markov chain-based text generation

--- plugins/waifu.py
+++ plugins/waifu.py
@@ -3,6 +3,8 @@ import aiohttp
 import random
 import discord
 from discord.ext import commands
+from utils.db import db
+from utils.markov_chain import MarkovChain
 
 class Waifu(commands.Cog):
     def __init__(self, bot):
@@ -23,5 +25,22 @@ class Waifu(commands.Cog):
                 img = io.BytesIO(await r.read())
                 await ctx.message.channel.send(file=discord.File(img, f'waifu-{num}.jpg', spoiler=True))
 
+class Markov(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def markov(self, ctx, user_mention):
+        user = ctx.message.guild.get_member(int(user_mention.strip("<>@!")))
+        curs = db.cursor()
+        statement = ("select content from messages where u_id=? and g_id=?")
+        text = ""
+        for message in curs.execute(statement, [user.id, ctx.guild.id]):
+            text += message[0].replace("\n", " ") + "\n"
+        mc = MarkovChain()
+        mc.train(text)
+        await ctx.send(mc.generate())
+
 def setup(bot):
     bot.add_cog(Waifu(bot))
+    bot.add_cog(Markov(bot))
---
+++ utils/markov_chain.py
@@ -0,0 +1,80 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from random import choices
+
+@dataclass
+class State:
+    word: str
+    occurences: int
+    probability: float
+
+    def __eq__(self, value):
+        return self.word == value
+    
+    def __repr__(self):
+        return f"<State word:{self.word}, occurences:{self.occurences}, probability:{self.probability}>"
+    
+    def __hash__(self):
+        return self.word.__hash__()
+
+
+class MarkovChain:
+    def __init__(self):
+        self.__states = defaultdict(list)
+        self.__start_state = State("", 0, 0.0)
+
+    def train(self, text):
+        lines = text.splitlines()
+        for line in lines:
+            words = line.split()
+            for idx, word in enumerate(words):
+                if idx == 0:
+                    if word in self.__states[self.__start_state]:
+                        self.__states[self.__start_state][self.__states[self.__start_state].index(word)].occurences += 1
+                    else:
+                        self.__states[self.__start_state].append(State(word, 1, 0.0))
+                try:
+                    next_word = words[idx + 1]
+                    if next_word in self.__states[word]:
+                        self.__states[word][self.__states[word].index(next_word)].occurences += 1
+                    else:
+                        self.__states[word].append(State(next_word, 1, 0.0))
+                except IndexError:
+                    self.__states[word].append(State("", 1, 1.0))
+            
+        for word, next_words in self.__states.items():
+            total_occurences = sum([w.occurences for w in next_words])
+            for next_word in next_words:
+                next_word.probability = next_word.occurences / total_occurences
+
+    def next(self, word):
+        if self.__states[word]:
+            return choices(self.__states[word], [w.probability for w in self.__states[word]])[0].word
+        else:
+            return ""
+
+    def generate(self, seed=None, length=None):
+        if seed:
+            generated = seed + " "
+        else:
+            seed = self.__start_state.word
+            generated = ""
+        count = 0
+        while True:
+            seed = self.next(seed)
+            if seed:
+                generated += seed + " "
+                count += 1
+                if length and count >= length:
+                    break
+            else:
+                break
+        return generated
+
+if __name__ == "__main__":
+    with open("messages.txt", "r") as f:
+        text = f.read()
+
+    mc = MarkovChain()
+    mc.train(text)
+    print(mc.generate())
