5d070ebbfd3fce84748c5334f7c52d299afcc9cf
Sergio Fenoll <sergio@fenoll.be>
2019-07-02 21:59:22 +0200

    Adds fuzzy search

--- plugins/waifu.py
+++ plugins/waifu.py
@@ -29,9 +29,37 @@ class Markov(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @staticmethod
+    def levenshtein(a, b):
+        v0 = list(range(len(b) + 1))
+        v1 = [0] * (len(b) + 1)
+        for i in range(len(a)):
+            v1[0] = i + 1
+            for j in range(len(b)):
+                deletion_cost = v0[j + 1] + 1
+                insertion_cost = v1[j] + 1
+                substitution_cost = v0[j] if a[i] == b[j] else v0[j] + 1
+                v1[j + 1] = min(deletion_cost, insertion_cost, substitution_cost)
+            v0 = v1[:]
+        return v0[len(b)]
+
+    @staticmethod
+    def fuzzy_match(term, member):
+        nick_dist = float("inf")
+        if member.nick:
+            nick_dist = Markov.levenshtein(term, member.nick)
+        name_dist = Markov.levenshtein(term, member.name)
+        return min(nick_dist, name_dist)
+
     @commands.command()
-    async def markov(self, ctx, user_mention):
-        user = ctx.message.guild.get_member(int(user_mention.strip("<>@!")))
+    async def markov(self, ctx, user_handle):
+        try:
+            user = ctx.message.guild.get_member(int(user_handle.strip("<>@!")))
+        except ValueError:
+            user = sorted([(m, self.fuzzy_match(user_handle, m)) for m in ctx.guild.members], key=lambda x: x[1])[0][0]
+        if not user:
+            await ctx.send(f"I couldn't find {user_handle}")
+            return
         curs = db.cursor()
         statement = ("select content from messages where u_id=? and g_id=?")
         text = ""
@@ -39,7 +67,10 @@ class Markov(commands.Cog):
             text += message[0].replace("\n", " ") + "\n"
         mc = MarkovChain()
         mc.train(text)
-        await ctx.send(mc.generate())
+        msg = mc.generate()
+        if msg == "":
+            msg = f"{user.nick} hasn't sent enough messages for me to generate anything..."
+        await ctx.send(msg)
 
 def setup(bot):
     bot.add_cog(Waifu(bot))
